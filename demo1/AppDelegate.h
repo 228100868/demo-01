6d72c2de65ccc97079e272356e64ab0dbfaa9f58
YzZ <228100868@qq.com>
2016-08-02 10:23:11 +0800

    Initial Commit

---
+++ demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by 禾家木 on 16/8/2.
+//  Copyright © 2016年 hejiamu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by 禾家木 on 16/8/2.
+//  Copyright © 2016年 hejiamu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
